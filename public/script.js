e3bc81182e201c2952808aa8dc1b231dbff89ff6
Enzo Coquelle <enzo.coquelle06@gmail.com>
2022-06-13 00:05:30 +0200

    minor changes

--- public/script.js
+++ public/script.js
@@ -1,18 +1,18 @@
-/*var socket = io();
+var socket = io();
 
-socket.on("new_photo", (msg) => {
+socket.on("new_photo", (name) => {
   var image_name = document.getElementById("image_name");
-  image_name.innerHTML = msg;
-});*/
-const imageUrl = " http://localhost:5555/public/D90-20220612234922.nef";
+  image_name.innerHTML = name;
+  const imageUrl = "photos/" + name;
 
-(async () => {
-  const response = await fetch(imageUrl);
-  var buf = new Uint8Array(await response.arrayBuffer());
-  var thumbnail = dcraw(buf, { extractThumbnail: true });
-  var blob = new Blob([thumbnail], { type: "image/jpeg" });
-  var urlCreator = window.URL || window.webkitURL;
-  var thumbnail_src = urlCreator.createObjectURL(blob);
-  var img = document.getElementById("img");
-  img.src = thumbnail_src;
-})();
+  (async () => {
+    const response = await fetch(imageUrl);
+    var buf = new Uint8Array(await response.arrayBuffer());
+    var thumbnail = dcraw(buf, { extractThumbnail: true });
+    var blob = new Blob([thumbnail], { type: "image/jpeg" });
+    var urlCreator = window.URL || window.webkitURL;
+    var thumbnail_src = urlCreator.createObjectURL(blob);
+    var img = document.getElementById("img");
+    img.src = thumbnail_src;
+  })();
+});
